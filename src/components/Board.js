d896699b2b170b3e3aef2c5aae255d7da5abea32
muskan <agarwal.muskan22@gmail.com>
2021-09-05 21:51:33 +0530

    edit board states and react effects

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setboard] = useState(Array(9).fill(null));
+  // null is default value for the array
+  // board is current value and setboard is updated value
+  const [isNext, setIsNext] = useState(false);
+  // false is default value
+  // isNext is current value and setIsNext is updated value
+  const handleSquareClick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setboard(prev => {
+      // prev here is the board array is use case that is the previous state or current state
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setIsNext(prev => !prev);
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
